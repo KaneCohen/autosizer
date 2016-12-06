43f5e2eb986b671b7493c222f284bfbcdb7bdbf5
Kane Cohen <kanecohen@gmail.com>
2016-12-06 12:05:39 -0800

    Add event emitter and add "updated" event.

--- dist/autosizer.js
+++ dist/autosizer.js
@@ -43,16 +43,15 @@ module.exports =
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	/**
 	 * Automatically resize textarea to fit text when typing.
-	 * autosizer 1.2.5
+	 * autosizer 1.3.0
 	 * Kane Cohen [KaneCohen@gmail.com] | https://github.com/KaneCohen
 	 * Copyright 2016 Kane Cohen <https://github.com/KaneCohen>
 	 * Available under BSD-3-Clause license
 	 */
-
 	'use strict';
 
 	Object.defineProperty(exports, '__esModule', {
@@ -61,7 +60,17 @@ module.exports =
 
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
+	var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var _events = __webpack_require__(1);
+
+	var _events2 = _interopRequireDefault(_events);
 
 	var _cloneStyles = {
 	  position: 'fixed',
@@ -93,11 +102,15 @@ module.exports =
 	  }
 	}
 
-	var Autosizer = (function () {
+	var Autosizer = (function (_EventEmitter) {
+	  _inherits(Autosizer, _EventEmitter);
+
 	  function Autosizer(el) {
 	    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
 
 	    _classCallCheck(this, Autosizer);
+
+	    _get(Object.getPrototypeOf(Autosizer.prototype), 'constructor', this).call(this);
 
 	    if (el.nodeName) {
 	      this.el = el;
@@ -205,6 +218,8 @@ module.exports =
 	        this.clone = null;
 	      }
 
+	      this.emit('updated', this);
+
 	      return this;
 	    }
 	  }, {
@@ -222,10 +237,16 @@ module.exports =
 	  }]);
 
 	  return Autosizer;
-	})();
+	})(_events2['default']);
 
 	exports['default'] = Autosizer;
 	module.exports = exports['default'];
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports = require("events");
 
 /***/ }
 /******/ ]);
